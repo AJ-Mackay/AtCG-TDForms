9de1b93993ec4a2f10da0777630f617907e65948
Paul Mackay <paul.m.mackay85@gmail.com>
2023-03-07 13:43:20 +0000

    Handling Radio Buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,8 @@ export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   defaultQuestion = 'pet';
   answer = '';
+  genders = ['male', 'female', 'other'];
+
 
   suggestUserName() {
     const suggestedName = 'Superuser';
